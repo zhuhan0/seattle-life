374f81f02b4a9db7453e4856251769e744da4264
Han Zhu <zhuhan7737@gmail.com>
2017-11-20 15:37:26 -0500

    add SearchBar text field

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import { AutoComplete, Paper, Toolbar, ToolbarGroup } from 'material-ui';
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: [],
+    };
+  }
+
   render() {
-    return <div>Hello world!</div>;
+    return (
+      <Paper zDepth={2}>
+        <Toolbar>
+          <ToolbarGroup>
+            <AutoComplete
+              hintText="Search anywhere in Seattle (city, address, zipcode, etc.)"
+              dataSource={this.state.dataSource}
+              textFieldStyle={{
+                width: '400px',
+              }}
+            />
+          </ToolbarGroup>
+        </Toolbar>
+      </Paper>
+    );
   }
 }
 
