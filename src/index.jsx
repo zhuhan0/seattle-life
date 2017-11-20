f7cd944a493dbbaa33bb912d3e56570da8795c66
Han Zhu <zhuhan7737@gmail.com>
2017-11-20 12:21:37 -0500

    set up webpack and babel

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+/* global document */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => <div>Hello world!</div>;
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var BUILD_DIR = path.resolve(__dirname, 'build');
+var APP_DIR = path.resolve(__dirname, 'src');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      { test: /\.(jsx)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'src/index.html',
+  })],
+};
+
+module.exports = config;
