7a39841a42d3533aba7019a25246cc084e7ef0d5
Han Zhu <zhuhan7737@gmail.com>
2017-12-07 14:11:12 -0500

    sort houses after getting search result

--- src/reducers/reducer_search.js
+++ src/reducers/reducer_search.js
@@ -1,9 +1,14 @@
+import _ from 'lodash';
 import { FETCH_DATA } from '../actions/index';
 
 export default function (state = [], action) {
   switch (action.type) {
-    case FETCH_DATA:
-      return action.payload.data;
+    case FETCH_DATA: {
+      const { data } = action.payload;
+      const sortedHouses = _.sortBy(data.houses, ['postcode']);
+      data.houses = sortedHouses;
+      return data;
+    }
     default:
       return state;
   }
