a2d0fcf0170a4a43ec88df879357862085bfffeb
Han Zhu <zhuhan7737@gmail.com>
2017-12-08 20:31:10 -0500

    filter crimes

--- src/reducers/reducer_search.js
+++ src/reducers/reducer_search.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import { FETCH_DATA } from '../actions/index';
 
-const features = [
+const utilities = [
   'Alternative Schools',
   'Beaches',
   'Cemeteries',
@@ -21,6 +21,14 @@ const features = [
   'Police Precincts',
 ];
 
+const crimes = [
+  'ASSAULTS',
+  'PERSONS - LOST, FOUND, MISSING',
+  'RESIDENTIAL BURGLARIES',
+  'ROBBERY',
+  'SEX OFFENSE (NO RAPE)',
+];
+
 export default function (state = [], action) {
   switch (action.type) {
     case FETCH_DATA: {
@@ -28,8 +36,11 @@ export default function (state = [], action) {
       const sortedHouses = _.sortBy(data.houses, ['postcode']);
       data.houses = sortedHouses;
 
-      const selectedUtilies = _.filter(data.utilities, utility => features.includes(utility['city feature']));
+      const selectedUtilies = _.filter(data.utilities, utility => utilities.includes(utility['city feature']));
       data.utilities = selectedUtilies;
+
+      const selectedCrimes = _.filter(data.crimes, crime => crimes.includes(crime.crime));
+      data.crimes = selectedCrimes;
       return data;
     }
     default:
