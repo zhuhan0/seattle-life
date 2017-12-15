16988e0c9ec0e1448e628194c188bea5f696ba1b
Yichen <syichen@seas.upenn.edu>
2017-12-15 10:49:15 -0500

    implemented crime infowindow

--- src/ResultBox/GMap.jsx
+++ src/ResultBox/GMap.jsx
@@ -196,6 +196,13 @@ class MapComponent extends React.Component {
             {marker.address}
           </div>
         );
+      } else if (marker.category === 3) {
+        infoWindow[marker._id] = (
+          <div style={{ overflow: 'hidden' }}>
+            {marker.crime}<br />
+            {marker.time}
+          </div>
+        );
       }
     });
 
